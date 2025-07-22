83ede28ca19ac6ade1b453f1482b94f3c363981a
Jobson Almeida <jobsonbox@gmail.com>
2025-07-22 16:18:56 -0300

    Lesson on format manipulation completed

--- section6.py
+++ section6.py
@@ -1,59 +1,109 @@
 import numpy as np
 
-array_3d = np.arange(1,25).reshape(2,4,3)
+# array_3d = np.arange(1,25).reshape(2,4,3)
 
-print(array_3d)
-print(type(array_3d[0,0,0]))
+# print(array_3d)
+# print(type(array_3d[0,0,0]))
 
-print(array_3d.tolist())
+# print(array_3d.tolist())
 
-print(array_3d.item(14))
-print(type(array_3d.item(14)))
+# print(array_3d.item(14))
+# print(type(array_3d.item(14)))
 
-array_3d.itemset(14, 50)
-print(array_3d)
-print(array_3d.item(14))
-print(type(array_3d.item(14)))
+# array_3d.itemset(14, 50)
+# print(array_3d)
+# print(array_3d.item(14))
+# print(type(array_3d.item(14)))
 
-array_3d.itemset((1,0,2), 100)
-print(array_3d)
+# array_3d.itemset((1,0,2), 100)
+# print(array_3d)
+
+# print("----")
+
+# array_3d = np.arange(1,25).reshape(2,4,3)
+# print(array_3d)
+# array_view = array_3d.view()
+# print(array_view)
+
+# array_view[0,0,0] = 0
+
+
+# print("---")
+# array_1 = np.array([1,2,3,4,5])
+# array_2 = array_1.view()
+# print(id(array_1))
+# print(id(array_2))
+# print(np.shares_memory(array_1, array_2))
+# print(id(array_1[0]))
+# print(id(array_2[0]))
+
+# # array_3 = array_1
+# # print(array_3 is array_1)
+# # print(id(array_3[0]))
+# # print(id(array_1[0]))
+# # print(np.shares_memory(array_1, array_2))
+
+# print("---")
+# array_3d = np.arange(1,25).reshape(2,4,3)
+# array_copy = array_3d.copy()
+# print(array_copy)
+# array_copy[0,0,0] = 99
+# print(array_copy)
+# print("array_3d: \n")
+# print(array_3d)
+
+# array_3d.fill(55)
+# print(array_3d)
+
+
+# print("----")
+# array_3d = np.arange(1,25)
+
+# B = array_3d.reshape(2,4,3, order='F')
+# print("B: \n")
+# print(B)
+
+# A = array_3d.reshape((4, 3, 2), order='F')
+# print("A: \n")
+# print(A)
+
+
+# print("share_memory: \n")
+# print(np.shares_memory(array_3d, B))
+# print(np.shares_memory(array_3d, A))
+# print(np.shares_memory(B, A))
+
+# C= array_3d.reshape(2,3, -1)
+# print(C)
+
+# print("---")
+
+# array_3d = np.arange(1,25).reshape(2, 4, 3)
+# print("array_3d:\n", array_3d)
+# array_transpose = array_3d.transpose((1,0,2))
+# print("array_transpose: \n", array_transpose)
+
+# array_3d_reshaped = array_3d.reshape((4,2,3))
+# print("array_3d_reshaped: \n", array_3d_reshaped)
+
+
+print("-----")
+
+array_2d = np.array([[1,2,3],
+                     [4,5,6]])
+print(array_2d.reshape(3,2, order='F'))
 
 print("----")
-
-array_3d = np.arange(1,25).reshape(2,4,3)
+array_3d = np.arange(1,25).reshape(2, 4, 3)
 print(array_3d)
-array_view = array_3d.view()
-print(array_view)
-
-array_view[0,0,0] = 0
-
-
 print("---")
-array_1 = np.array([1,2,3,4,5])
-array_2 = array_1.view()
-print(id(array_1))
-print(id(array_2))
-print(np.shares_memory(array_1, array_2))
-print(id(array_1[0]))
-print(id(array_2[0]))
+array_flatten = array_3d.flatten(order='C')
+print(np.shares_memory(array_3d, array_flatten))
+array_ravel = array_3d.ravel()
+print(np.shares_memory(array_3d, array_ravel))
 
-# array_3 = array_1
-# print(array_3 is array_1)
-# print(id(array_3[0]))
-# print(id(array_1[0]))
-# print(np.shares_memory(array_1, array_2))
+array_axis_size = array_3d.reshape(8,1,3)
+print(array_axis_size)
+print(array_axis_size.squeeze())
 
-print("---")
-array_3d = np.arange(1,25).reshape(2,4,3)
-array_copy = array_3d.copy()
-print(array_copy)
-array_copy[0,0,0] = 99
-print(array_copy)
-print("array_3d: \n")
-print(array_3d)
-
-array_3d.fill(55)
-print(array_3d)
-
-
-
+# print( array_3d.swapaxes(1,0) )
