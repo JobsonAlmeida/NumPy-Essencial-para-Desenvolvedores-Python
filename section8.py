6df762ec5379c342f4b2bd42d2efea54ec32d679
Jobson Almeida <jobsonbox@gmail.com>
2025-07-26 13:42:23 -0300

    Completed lesson on rearranging array

---
+++ section8.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+array_1d = np.arange(0,10)
+array_2d_0 = np.arange(0,18).reshape(3,6)
+array_2d_1 = np.arange(18,36).reshape(3,6)
+
+# print(array_2d_0)
+# print("---")
+# print(array_2d_1)
+
+# print("---")
+# array_concatenated = np.concatenate([array_2d_0, array_2d_1], axis=1)
+# print(array_concatenated)
+# print("---")
+# array_stacked = np.stack(arrays=[array_2d_0, array_2d_1])
+# print(array_stacked)
+# print("---")
+# array_stacked = np.stack(arrays=[array_2d_0, array_2d_1], axis=1)
+# print(array_stacked)
+# print("---")
+# array_stacked = np.stack(arrays=[array_2d_0, array_2d_1], axis=-1)
+# print(array_stacked)
+# print(array_stacked.shape)
+
+# print("---")
+# array_vstack = np.vstack(tup= [array_2d_0, array_2d_1])
+# print(array_vstack)
+
+# print("---")
+# array_vstack = np.hstack(tup= [array_2d_0, array_2d_1])
+# print(array_vstack)
+
+# print("---array 1d --- ")
+# print(array_1d)
+# array_split = np.split(ary=array_1d, indices_or_sections=2)
+# print(array_split)
+# print(len(array_split))
+# print(array_split[0])
+
+# print("--- array 2d ---")
+# print(array_2d_0)
+# array_split = np.split(ary=array_2d_0, indices_or_sections=3, axis=1)
+# print(array_split)
+# print(array_split[0])
+
+# print("--- array 2d ---")
+# print(array_2d_0)
+# array_split = np.split(ary=array_2d_0, indices_or_sections=[2,3], axis=1)
+# print(array_split)
+# print(array_split[0])
+
+# print('---')
+# print(array_2d_0)
+# print(array_2d_0.ndim, array_2d_0.shape)
+# new_array_2d_0 = np.expand_dims(a = array_2d_0, axis=0)
+# print(new_array_2d_0)
+# print(new_array_2d_0.ndim, new_array_2d_0.shape)
+# new_array_2d_0_after_squeeze = np.squeeze(new_array_2d_0)
+# print(new_array_2d_0_after_squeeze)
+
+
+# print('---')
+# print(array_2d_0)
+# print(array_2d_0.ndim, array_2d_0.shape)
+# new_array_2d_0 = np.expand_dims(a = array_2d_0, axis=1)
+# print(new_array_2d_0)
+# print(new_array_2d_0.ndim, new_array_2d_0.shape)
+# new_array_2d_0_after_squeeze = np.squeeze(new_array_2d_0)
+# print(new_array_2d_0_after_squeeze)
+
+# print("----")
+
+# print(array_2d_0[np.newaxis, :])
+# print(array_2d_0[:, np.newaxis, :])
+# print(array_2d_0.reshape(3,6,1))
+
+# print("---")
+# print(array_1d)
+# print(np.flip(array_1d))
+# print(array_1d[::-1])
+
+print("---array original---")
+print(array_2d_0)
+# print("---array sem axis---")
+# print(np.flip(array_2d_0))
+
+# print("--- flip axis=0---")
+# print(np.flip(array_2d_0, axis=0))
+print("---flip axis=1---")
+print(np.flip(array_2d_0, axis=1))
+
+print("---")
+print(np.resize(a=array_2d_0, new_shape=12))
+print(np.resize(a=array_2d_0, new_shape=(2,5,3)))
+array_2d_0.reshape(2,3)
